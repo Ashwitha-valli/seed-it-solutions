caf5e0c9beb837b189538534231e449be1d6c628
Ashwitha Kannan <ashwithak437@gmail.com>
2026-09-17 22:31:35 +0530

    Solve: [Q0.144] Basic_Level_0_Arrays_2 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-144-basic-level-0-arrays-2/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Problem: Q0.144 - Basic_Level_0_Arrays_2
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-17
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+      int sum=0;
+      Scanner sc = new Scanner(System.in);
+      int n=sc.nextInt();
+      int[] arr=new int[n];
+      for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+      }
+      for(int num:arr){
+        sum+=num;
+      }
+      System.out.println(sum);
+    }
+}
