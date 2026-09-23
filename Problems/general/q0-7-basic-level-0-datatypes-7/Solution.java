7cdd99b36233eb862948d88568684c62b3ceb516
Ashwitha Kannan <ashwithak437@gmail.com>
2026-09-23 09:07:14 +0530

    Solve: [Q0.7] Basic_level_0_Datatypes_7 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-7-basic-level-0-datatypes-7/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Problem: Q0.7 - Basic_level_0_Datatypes_7
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-23
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+        //int min=0;
+        Scanner sc = new Scanner(System.in);
+        float n = sc.nextFloat();
+        /*int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        for(int num:arr){
+            min+=num;
+        }*/ 
+        //float n=17;
+        System.out.printf("%.3f",n);
+    }
+}
