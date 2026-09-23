ae485c0593a17cbea390ce3fb5665b8d3f528b4c
Ashwitha Kannan <ashwithak437@gmail.com>
2026-09-23 08:52:24 +0530

    Solve: [Q0.152] Basic_Level_0_Arrays_10 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-152-basic-level-0-arrays-10/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Problem: Q0.152 - Basic_Level_0_Arrays_10
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-23
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+        int min=0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        for(int num:arr){
+            min+=num;
+        }
+        System.out.println(min/n);
+    }
+}
