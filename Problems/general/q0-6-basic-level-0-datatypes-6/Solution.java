baa5956db1c3b01458ad4dad27931c18113c1c67
Ashwitha Kannan <ashwithak437@gmail.com>
2026-09-23 09:03:24 +0530

    Solve: [Q0.6] Basic_level_0_Datatypes_6 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-6-basic-level-0-datatypes-6/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Problem: Q0.6 - Basic_level_0_Datatypes_6
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-23
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+        //int min=0;
+        Scanner sc = new Scanner(System.in);
+        char n = sc.next().charAt(0);
+        /*int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        for(int num:arr){
+            min+=num;
+        }*/ 
+        //int n=17;
+        System.out.println(n);
+    }
+}
