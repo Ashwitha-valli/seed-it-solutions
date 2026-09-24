e67ff130b89f5e6877e443e4362c897596eb5ab9
Ashwitha Kannan <ashwithak437@gmail.com>
2026-09-24 09:14:17 +0530

    Solve: [Q0.8] Basic_level_0_Datatypes_8 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-8-basic-level-0-datatypes-8/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Problem: Q0.8 - Basic_level_0_Datatypes_8
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-24
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+        String s ;
+
+        Scanner sc = new Scanner(System.in);
+        s = sc.nextLine();
+        System.out.println(s);
+    }
+}
