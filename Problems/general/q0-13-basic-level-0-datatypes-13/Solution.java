fb709d5b5d4c87676215d0801fc91adfc4e5403e
Ashwitha Kannan <ashwithak437@gmail.com>
2026-09-24 10:53:24 +0530

    Solve: [Q0.13] Basic_level_0_Datatypes_13 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-13-basic-level-0-datatypes-13/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Problem: Q0.13 - Basic_level_0_Datatypes_13
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-24
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+        float s;
+        Scanner sc = new Scanner(System.in);
+        s=sc.nextFloat();
+        System.out.println(s*s*3.14);
+
+    }
+}
