92a9ee2aa81e7c3e899abdfff52e6a33218f7dcb
Ashwitha Kannan <ashwithak437@gmail.com>
2026-09-24 11:12:53 +0530

    Solve: [Q0.15] Basic_level_0_Datatypes_15 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-15-basic-level-0-datatypes-15/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Problem: Q0.15 - Basic_level_0_Datatypes_15
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-24
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+         float s;
+        Scanner sc = new Scanner(System.in);
+        s=sc.nextFloat();
+        int v  =(int)s;
+        System.out.println(v);
+        
+    }
+}
