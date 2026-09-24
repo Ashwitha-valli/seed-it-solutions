31724cfce2defa5f35f6b11fab4fb4ffae3c1b3f
Ashwitha Kannan <ashwithak437@gmail.com>
2026-09-24 09:22:33 +0530

    Solve: [Q0.11] Basic_level_0_Datatypes_11 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-11-basic-level-0-datatypes-11/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Problem: Q0.11 - Basic_level_0_Datatypes_11
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-24
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+        int n;int x;
+
+        Scanner sc =new Scanner(System.in);
+        n=sc.nextInt();
+        x=sc.nextInt();
+        System.out.println(n+x);
+    }
+}
