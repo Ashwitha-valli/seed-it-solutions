6bf17643c9e94054e73f65933af455827551262b
Ashwitha Kannan <ashwithak437@gmail.com>
2026-09-25 16:14:53 +0530

    Solve: [Q0.16] Basic_level_0_Datatypes_16 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-16-basic-level-0-datatypes-16/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Problem: Q0.16 - Basic_level_0_Datatypes_16
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-25
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // your code goes 
+        int n ;
+        int s;
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        s=sc.nextInt();
+        System.out.printf("%d%017d",n,s);
+    }
+}
